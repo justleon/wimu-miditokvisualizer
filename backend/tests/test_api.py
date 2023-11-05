4d95797f07b86beb3e5e378c1fcdc44a48aaeb90
Michał Wiszenko <wiszenkom@gmail.com>
2023-11-05 22:21:28 +0100

    Fix tests

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,5 +1,26 @@
+import os
 import pytest
-from core.api.api import read_root
+from fastapi.testclient import TestClient
+from core.api.api import app, process
+from core.constants import DATA_DIR
 
-def test_capital_case():
-    assert read_root()
+MIDI_FILE_NAME = "example.mid"
+MIDI_FILE_PATH = os.path.join(DATA_DIR, MIDI_FILE_NAME)
+
+client = TestClient(app)
+
+
+@pytest.fixture
+def event_loop():
+    import asyncio
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.mark.asyncio
+async def test_upload_file():
+    with open(MIDI_FILE_PATH, "rb") as file:
+        form_data = {"file": file}
+        response = client.post("/process", files=form_data)
+        assert response.status_code == 200
