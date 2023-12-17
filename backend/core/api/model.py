657753ea49c8adf066c440861a5b8c9b8c319888
Michał Wiszenko <wiszenkom@gmail.com>
2023-12-17 12:11:23 +0100

    Add TODO notes

--- backend/core/api/model.py
+++ backend/core/api/model.py
@@ -4,10 +4,11 @@ from typing_extensions import Annotated
 import json
 
 
-class ConfigModel(BaseModel):
+class ConfigModel(BaseModel): # TODO: beat_res, beat_res_rest, chord_maps, chord_tokens_with_root_note, chord_unknown, time_signature_range
     tokenizer: Literal['REMI', 'REMIPlus', 'MIDILike', 'TSD', 'Structured', 'CPWord', 'Octuple', 'MuMIDI', 'MMM']
     pitch_range: Annotated[list[Annotated[int, Field(ge=0, le=127)]], Field(min_items=2, max_items=2)]
     nb_velocities: Annotated[int, Field(ge=0, le=127)]
+    special_tokens: list[str]
     use_chords: StrictBool
     use_rests: StrictBool
     use_tempos: StrictBool
@@ -17,13 +18,12 @@ class ConfigModel(BaseModel):
     use_programs: StrictBool
     nb_tempos: NonNegativeInt
     tempo_range: Annotated[list[NonNegativeInt], Field(min_items=2, max_items=2)]
-    special_tokens: list[str]
     log_tempos: StrictBool
     delete_equal_successive_tempo_changes: StrictBool
+    sustain_pedal_duration: StrictBool
+    pitch_bend_range: Annotated[list[int], Field(min_items=3, max_items=3)]
     delete_equal_successive_time_sig_changes: StrictBool
-    pitch_bend_range: Annotated[list[int], Field(min_items=2, max_items=2)]
-    pitch_bend_range_number: int
-    programs: Optional[Annotated[list[int], Field(min_items=2, max_items=2)]]
+    programs: Optional[Annotated[list[int], Field(min_items=1)]]
     one_token_stream_for_programs: Optional[StrictBool]
     program_changes: Optional[StrictBool]
 
--- backend/core/service/midi_processing.py
+++ backend/core/service/midi_processing.py
@@ -10,16 +10,25 @@ def tokenize_midi_file(user_config: ConfigModel, midi_bytes: bytes):
         "pitch_range": tuple(user_config.pitch_range),
         "beat_res": {(0, 4): 8, (4, 12): 4},
         "nb_velocities": user_config.nb_velocities,
-        "special_tokens": ["PAD", "BOS", "EOS", "MASK"],
+        "special_tokens": user_config.special_tokens,
         "use_chords": user_config.use_chords,
         "use_rests": user_config.use_rests,
         "use_tempos": user_config.use_tempos,
         "use_time_signatures": user_config.use_time_signatures,
-        "use_programs": user_config.use_programs,
         "use_sustain_pedals": user_config.use_sustain_pedals,
         "use_pitch_bends": user_config.use_pitch_bends,
         "nb_tempos": user_config.nb_tempos,
         "tempo_range": tuple(user_config.tempo_range),
+        "log_tempos": user_config.log_tempos,
+        "delete_equal_successive_tempo_changes": user_config.delete_equal_successive_tempo_changes,
+        "sustain_pedal_duration": user_config.sustain_pedal_duration,
+        "pitch_bend_range": user_config.pitch_bend_range,
+        "delete_equal_successive_time_sig_changes": user_config.delete_equal_successive_time_sig_changes,
+        # TODO: dynamic config preparation as not all tokenizers are compatible with program parameters
+        # "use_programs": user_config.use_programs,
+        # "programs": user_config.programs,
+        # "one_token_stream_for_programs": user_config.one_token_stream_for_programs,
+        # "program_changes": user_config.program_changes,
     }
     tokenizer_config = TokenizerConfig(**tokenizer_params)
 
