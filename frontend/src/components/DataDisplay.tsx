91bf566f18eb10bbbfa50617b42390bdb492166e
justleon <maniak656@gmail.com>
2024-01-14 22:28:15 +0100

    Removed sample value Adjusted piano roll blocks Formatting changes

--- frontend/src/components/DataDisplay.tsx
+++ frontend/src/components/DataDisplay.tsx
@@ -4,21 +4,17 @@ import PianoRollBlock from './PianoRollBlock';
 
 interface DataDisplayProps {
   data: NestedList<Token>;
-  sample: number;
 }
 
-const DataDisplay: React.FC<DataDisplayProps> = ({ data, sample }) => {
+const DataDisplay: React.FC<DataDisplayProps> = ({ data }) => {
   const renderNestedList = <T extends Token>(
     list: NestedList<T>,
-    n: number,
     level: number,
     parentIndex: number[]
-  ): React.ReactNode => {
-    const slicedList = list.slice(0, n);
-  
+  ): React.ReactNode => {  
     return (
       <>
-        {slicedList.map((item, index) => {
+        {list.map((item, index) => {
           const currentIndex = [...parentIndex, index + 1];
           const heading = currentIndex.join('.');
   
@@ -27,7 +23,7 @@ const DataDisplay: React.FC<DataDisplayProps> = ({ data, sample }) => {
             {Array.isArray(item) ? (
               <>
                 <div>{heading}</div>
-                {renderNestedList(item, n, level + 1, currentIndex)}
+                {renderNestedList(item, level + 1, currentIndex)}
               </>
             ) : (
               <PianoRollBlock key={index} item={item as Token} heading={heading} />
@@ -44,7 +40,7 @@ const DataDisplay: React.FC<DataDisplayProps> = ({ data, sample }) => {
     return <div>Invalid data</div>;
   }
 
-  return <div>{renderNestedList(data, sample, 0, [])}</div>;
+  return <div>{renderNestedList(data, 0, [])}</div>;
 };
 
 export default DataDisplay;
