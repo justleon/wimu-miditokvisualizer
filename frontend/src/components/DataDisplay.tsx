28b2595d59e5edef5441ea4bab19c0ce96f32441
Konstantin Panov <konstantin.n.panov@gmail.com>
2024-04-11 18:15:02 +0200

    feat: Separate token info from token blocks

--- frontend/src/components/DataDisplay.tsx
+++ frontend/src/components/DataDisplay.tsx
@@ -1,46 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Token, NestedList } from '../interfaces/ApiResponse';
 import PianoRollBlock from './PianoRollBlock';
+import TokenInfo from './TokenInfo';
 
 interface DataDisplayProps {
   data: NestedList<Token>;
 }
 
-const DataDisplay: React.FC<DataDisplayProps> = ({ data }) => {
-  const renderNestedList = <T extends Token>(
-    list: NestedList<T>,
-    level: number,
-    parentIndex: number[]
-  ): React.ReactNode => {  
-    return (
-      <>
-        {list.map((item, index) => {
-          const currentIndex = [...parentIndex, index + 1];
-          const heading = currentIndex.join('.');
-  
-          return (
-            <div key={index} style={{ display: 'flex', marginRight: '10px' }}>
+
+const RNestedList: React.FC<{
+  onHover: (t: Token | null, heading: string) => void;
+  list: NestedList<Token>;
+  level: number;
+  parentIndex: number[];
+}> = ({ onHover, list, level, parentIndex }) => {
+  return (
+    <>
+      {list.map((item, index) => {
+        const currentIndex = [...parentIndex, index + 1];
+        const heading = currentIndex.join('.');
+
+        return (
+          <div key={index} style={{ display: 'flex', marginRight: '10px' }}>
             {Array.isArray(item) ? (
               <>
-                <div style={{marginRight: '5px'}}>{heading}</div>
-                {renderNestedList(item, level + 1, currentIndex)}
+                <div style={{ marginRight: '5px' }}>{heading}</div>
+                <RNestedList
+                  onHover={onHover}
+                  list={item} level={level + 1} parentIndex={currentIndex} />
               </>
             ) : (
-              <PianoRollBlock key={index} item={item as Token} heading={heading} />
+              <PianoRollBlock
+                onHover={onHover}
+                key={index} item={item as Token} heading={heading} />
             )}
           </div>
-          );
-        })}
-        <br />
-      </>
-    );
-  };  
+        );
+      })}
+      <br />
+    </>
+  );
+}
+
+const DataDisplay: React.FC<DataDisplayProps> = ({ data }) => {
+
+  const [token, setToken] = useState<Token | null>(null);
+  const [heading, setHeading] = useState<string>("");
+
+  const updateTokenInfo = (token: Token | null, heading: string) => {
+    setToken(token);
+    setHeading(heading);
+  }
 
   if (!Array.isArray(data)) {
     return <div>Invalid data</div>;
   }
 
-  return <div>{renderNestedList(data, 0, [])}</div>;
+  return <div>
+    <TokenInfo token={token} heading={heading} />
+    <RNestedList
+      onHover={updateTokenInfo}
+      list={data} level={0} parentIndex={[]} />
+  </div>;
 };
 
 export default DataDisplay;
--- frontend/src/components/PianoRollBlock.tsx
+++ frontend/src/components/PianoRollBlock.tsx
@@ -1,96 +1,61 @@
-import React, { useState } from 'react';
+import React, { memo } from 'react';
 import { Token } from '../interfaces/ApiResponse';
 
 interface PianoRollBlockProps {
   item: Token;
+  onHover: (t: Token | null, heading: string) => void;
   heading: string; // Add heading as a prop
 }
 
-const PianoRollBlock: React.FC<PianoRollBlockProps> = ({ item, heading }) => {
-  const [isHovered, setHovered] = useState(false);
+export function TokenTypeToColor(type: string) {
+  switch (type) {
+    case 'Bar': return '#67948b';
+    case 'Position':
+    case 'TimeShift': return '#748a9a';
+    case 'Tempo':
+    case 'Rest': return '#87673e';
+    case 'Pitch':
+    case 'NoteOn': return '#778b68';
+    case 'Velocity': return '#cc968e';
+    case 'Duration':
+    case 'NoteOff': return '#4c4732';
+    case 'Family':
+    case 'TimeSig': return '#b076b2';
+    default: return '#000000'
+  }
+}
+
+
+const PianoRollBlock: React.FC<PianoRollBlockProps> = memo(({ item, onHover, heading }) => {
 
   const handleMouseEnter = () => {
-    setHovered(true);
+    onHover(item, heading);
   };
-
   const handleMouseLeave = () => {
-    setHovered(false);
-  };
-  
-  const dynamicFill = (type: string) => {
-    if (type === 'Bar') {
-      return '#67948b'
-    }
-    else if (type === 'Position' || type === 'TimeShift') {
-      return '#748a9a'
-    }
-    else if (type === 'Tempo' || type === 'Rest') {
-      return '#87673e'
-    }
-    else if (type === 'Pitch' || type === 'NoteOn') {
-      return '#778b68'
-    }
-    else if (type === 'Velocity') {
-      return '#cc968e'
-    }
-    else if (type === 'Duration' || type === 'NoteOff') {
-      return '#4c4732'
-    }
-    else if (type === 'Family' || type === 'TimeSig') {
-      return '#b076b2'
-    }
-    else {
-      return '#000000'
-    }
+    onHover(null, "");
   };
 
   return (
-      <div
-        style={{
-          display: 'inline-block',
-          width: '70px',
-          height: '100px',
-          border: '1px solid #ccc',
-          margin: '5px',
-          position: 'relative',
-          backgroundColor: dynamicFill(item.type)
-        }}
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-          {isHovered && (
-            <>
-              <div style={{ fontSize: '12px', marginBottom: '5px' }}>
-                <b>{heading}</b>
-              </div>
-              <div style={{ fontSize: '10px' }}>
-                <strong>Type:</strong> {item.type}
-              </div>
-              <div style={{ fontSize: '10px' }}>
-                <strong>Value:</strong> {item.value}
-              </div>
-              <div style={{ fontSize: '10px' }}>
-                <strong>Time:</strong> {item.time}
-              </div>
-              <div style={{ fontSize: '10px' }}>
-                <strong>Program:</strong> {item.program}
-              </div>
-              <div style={{ fontSize: '10px' }}>
-                <strong>Desc:</strong> {item.desc}
-              </div>
-            </>
-          )}
-          {!isHovered && (
-            <>
-              <div style={{ fontSize: '10px' }}>
-                <strong>{item.type}</strong>
-              </div>
-            </>
-          )}
+    <div
+      style={{
+        display: 'inline-block',
+        width: '70px',
+        height: '100px',
+        border: '1px solid #ccc',
+        margin: '5px',
+        position: 'relative',
+        backgroundColor: TokenTypeToColor(item.type)
+      }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
+        <div style={{ fontSize: '10px' }}>
+          <strong>{item.type}</strong>
         </div>
       </div>
-    );
-};
+    </div>
+  );
+});
 
 export default PianoRollBlock;
